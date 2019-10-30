41a964e77becd6eb28efa5f599bd96ff4bb4ad44
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2019-10-29 22:33:18 -0400

    setting up folders

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 8889, // 3306 for windows
+    uname: "root",
+    pword: "root", // leave blank for windows
+    database: "my_favourite_things"
+}
+
+module.exports = config;
